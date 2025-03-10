8e78b7699051e50de91e9e6927c9e7b73bab49a9
Dii11 <mandimbiarisatad@gmail.com>
2025-03-10 22:24:55 +0300

    maj routage user

--- frontend/src/user/components/MenuUser.jsx
+++ frontend/src/user/components/MenuUser.jsx
@@ -1,24 +1,20 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import IconButton from '@mui/material/IconButton';
-import { History, Home, Money } from '@mui/icons-material';
+import { Home, Money } from '@mui/icons-material';
 
-const Menu = () => {
-    return (
-        <div className='lien'>
-            <NavLink to="/user/etablissements" >
-                    <Home />
-            </NavLink>
+const MenuUser = () => {
+  return (
+    <div className='lien'>
+      <NavLink to="/user/etablissements" >
+        <IconButton><Home /></IconButton>
+      </NavLink>
 
-            <NavLink to="/user/depenses" >
-                    <Money />
-            </NavLink>
-
-            <NavLink className="audit" to="/user/audit" >
-                    <History />
-            </NavLink>
-        </div>
-    );
+      <NavLink to="/user/depenses" >
+        <IconButton><Money /></IconButton>
+      </NavLink>
+    </div>
+  );
 };
 
-export default Menu;
+export default MenuUser;
